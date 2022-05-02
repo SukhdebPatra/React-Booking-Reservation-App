6880ed542398f9e0043fe3f2bdeaac689bb9fe0a
SukhdebPatra <95843732+SukhdebPatra@users.noreply.github.com>
2022-05-02 21:52:09 +0530

    Add files via upload
    Reservation App

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../featured/Featured'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div>
+    
+        <Navbar/>
+        <Header/>
+        <div className='homeContainer'>
+            <Featured/>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Home
